cb3d68db64808cce6875829c5f9d7b20c7cadb2e
M. Raddaoui <140417247+mr-hookie-repo@users.noreply.github.com>
2024-08-25 18:40:08 -0400

    new logic to make the code run faster and added comments

--- src/utils/DeliveryCalculator.js
+++ src/utils/DeliveryCalculator.js
@@ -1,8 +1,26 @@
 class DeliveryCalculator {
+    static DELIVERY_CHARGES = [
+      { threshold: 90, charge: 0 },
+      { threshold: 50, charge: 2.95 },
+      { threshold: 0, charge: 4.95 }
+    ];
+
+    // Calculate the delivery charge based on the total price of the items in the basket.
+   
     static calculate(total) {
-      if (total >= 90) return 0;
-      if (total >= 50) return 2.95;
-      return 4.95;
+      /**
+       * The rules for calculating the delivery charge are as follows:
+       *  - Delivery is free for orders over 90.
+       *  - Delivery is 2.95 for orders over 50.
+       *  - Delivery is 4.95 for orders under 50.
+       */
+      for (const { threshold, charge } of this.DELIVERY_CHARGES) {
+        if (total >= threshold) {
+          return charge;
+        }
+      }
+
+      throw new Error('Unreachable code');
     }
   }
   
